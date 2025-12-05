7dd49ed8e08b43e99efff6ab9c01668edb456fd4
akolliasAMD <akollias@amd.com>
2025-12-04 19:57:45 -0600

    cleanNontermporalStore

--- src/assembly.hpp
+++ src/assembly.hpp
@@ -221,59 +221,18 @@ __device__ __forceinline__ void store_asm(uint8_t* val, uint8_t* dst,
                                           int size) {
   switch (size) {
     case 2: {
-#if defined(__gfx906__)
-#endif
-#if defined(__gfx908__)
-#endif
-#if defined(__gfx90a__)
       int16_t val16{*(reinterpret_cast<int16_t*>(val))};
-      asm volatile("flat_store_short %0 %1 glc slc" : : "v"(dst), "v"(val16));
-#endif
-#if defined(__gfx942__) || defined(__gfx950__)
       __builtin_nontemporal_store(val16, (int16_t *)dst);
-#endif
-#if defined(__gfx1100__)
-      int32_t val32{*(reinterpret_cast<int32_t*>(val))};
-      asm volatile("flat_store_short %0 %1 glc slc" : : "v"(dst), "v"(val32));
-#endif
-#if defined(__gfx1201__)
-      int32_t val32{*(reinterpret_cast<int32_t*>(val))};
-      asm volatile("flat_store_b16 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val32));
-#endif
       break;
     }
     case 4: {
       int32_t val32{*(reinterpret_cast<int32_t*>(val))};
-#if defined(__gfx906__)
-#endif
-#if defined(__gfx908__)
-#endif
-#if defined(__gfx90a__) || defined(__gfx1100__)
-      asm volatile("flat_store_dword %0 %1 glc slc" : : "v"(dst), "v"(val32));
-#endif
-#if defined(__gfx942__) || defined(__gfx950__)
       __builtin_nontemporal_store(val32, (int32_t *)dst);
-#endif
-#if defined(__gfx1201__)
-      asm volatile("flat_store_b32 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val32));
-#endif
       break;
     }
     case 8: {
       int64_t val64{*(reinterpret_cast<int64_t*>(val))};
-#if defined(__gfx906__)
-#endif
-#if defined(__gfx908__)
-#endif
-#if defined(__gfx90a__) || defined(__gfx1100__)
-      asm volatile("flat_store_dwordx2 %0 %1 glc slc" : : "v"(dst), "v"(val64));
-#endif
-#if defined(__gfx942__) || defined(__gfx950__)
       __builtin_nontemporal_store(val64, (int64_t *)dst);
-#endif
-#if defined(__gfx1201__)
-      asm volatile("flat_store_b64 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val64));
-#endif
       break;
     }
     default:
